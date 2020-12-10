a70399d5226f274b82accad2ed4206da8f81ae50
Roberto Milani <milani@strands.com>
2020-12-10 15:32:09 +0100

    added descriptive info

--- src/components/App.js
+++ src/components/App.js
@@ -22,6 +22,11 @@ class App extends React.Component {
 	render() {
 		return(
 			<div className="ui container" style={{marginTop: "20px"}}>
+				<h1 className="ui header">Image Search</h1>
+				<p1>This is a very simple React app which uses a controlled component (the search bar) to receive an input search text from the user. 
+					The input text is used to perform an image search through the Unsplash API, and the images are presented in an ordered grid pattern.
+					The React reference feature is used for each image card in order to format the grid properly.
+				</p1>
 				<SearchBar onSubmit={this.onSearchSubmit}/>
 				Displaying the first {this.state.images.length} images
 				<ImageList images={this.state.images}/>
